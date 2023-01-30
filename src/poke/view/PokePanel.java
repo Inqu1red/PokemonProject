55d39db611e8984e4a2239ea51d5d8cf5dc07128
Inqu1red <dylangibbins8@gmail.com>
2023-01-30 12:15:14 -0700

    Update PokePanel.java

--- src/poke/view/PokePanel.java
+++ src/poke/view/PokePanel.java
@@ -3,7 +3,17 @@ package poke.view;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.ImageIcon;
 
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import poke.controller.Controller;
 
 public class PokePanel extends JPanel
@@ -11,7 +21,17 @@ public class PokePanel extends JPanel
 	private Controller app;
 	
 	private SpringLayout layout;
-
+	
+	private JTextField nameField;
+	private JTextField healthField;
+	private JTextField numberField;
+	
+	private JCheckBox evolution;
+	
+	private JTextArea typesArea;
+	private JScrollPane typePane;
+	
+	private JLabel 
 	
 	public PokePanel(Controller app)
 	{
