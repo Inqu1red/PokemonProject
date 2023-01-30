976218ccb1c3a3999c3d33f1e69bf50793599cb9
Inqu1red <dylangibbins8@gmail.com>
2023-01-30 11:44:09 -0700

    added pokedex method and pokemonList

--- src/poke/controller/Controller.java
+++ src/poke/controller/Controller.java
@@ -1,11 +1,16 @@
 package poke.controller;
 
 
+import java.util.ArrayList;
+
 import poke.model.*;
 
 public class Controller
 {
 
+	ArrayList<Pokemon> pokemonList = new ArrayList<Pokemon>();
+	
+	String dataFile = "";
 	
 	public Controller()
 	{
@@ -17,5 +22,31 @@ public class Controller
 		
 	}
 	
+	public Pokemon pokedex()
+	{
+		Pokemon poke = null;
+		
+		
+		createPokedex();
+		
+		
+		return poke;
+	}
+	
+	
+	
+	private void createPokedex()
+	{
+		pokemonList.add(new Gengar());
+		pokemonList.add(new Lucario());
+		pokemonList.add(new Sprigatitio());
+		pokemonList.add(new Rowlet());
+		pokemonList.add(new Pichu());
+		pokemonList.add(new Gengar(120, "Sean124"));
+		pokemonList.add(new Rowlet("little guy"));
+		pokemonList.add(new Sprigatitio(940, "Grass Cat"));
+		pokemonList.add(new Pichu("Small rodent"));
+		pokemonList.add(new Lucario(345, "Big Guy"));
+	}
 	
 }
